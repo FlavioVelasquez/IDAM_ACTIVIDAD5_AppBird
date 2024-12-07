247a58c3cb7ff30053f0bf2540b41dabc7e41235
FlavioVelasquez <22000390@galileo.edu>
2024-12-07 07:47:08 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppBird',
+  webDir: 'www'
+};
+
+export default config;
